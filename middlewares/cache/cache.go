9731c866a1abe77d5dfd82dc57d14f011b2ab4e2
Felipe A. Cavani <fcavani@gmail.com>
2017-10-10 09:53:17 -0300

    Set insted Add... Maybe will have more than one.

--- middlewares/cache/cache.go
+++ middlewares/cache/cache.go
@@ -193,7 +193,7 @@ func (s *Storage) ReadTo(d Document, w http.ResponseWriter) error {
 
 	for k, v := range h {
 		for _, item := range v {
-			w.Header().Set(k, item)
+			w.Header().Add(k, item)
 		}
 	}
 
