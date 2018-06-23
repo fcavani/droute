8ea51990a819e6290f44849a2501cd3d548d128b
Felipe A. Cavani <fcavani@gmail.com>
2018-06-23 08:56:54 -0300

    ...

--- middlewares/fileserver/stripprefix.go
+++ middlewares/fileserver/stripprefix.go
@@ -1,19 +1,20 @@
 package fileserver
 
 import (
+	"net/http"
 	"net/url"
 	"strings"
 
 	"gopkg.in/fcavani/httprouter.v2"
 )
 
-func StripPrefix(prefix string, h Handler) Handler {
+func StripPrefix(prefix string, h Handler) http.Handler {
 	if prefix == "" {
 		return h
 	}
-	return HandlerFunc(func(w ResponseWriter, r *Request) {
+	return HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
-		lang := httprouter.ContentLang()
+		lang := httprouter.ContentLang(req)
 		if lang != "" {
 			p := strings.TrimPrefix(path, "/"+lang)
 			if len(p) < len(path) {
@@ -21,14 +22,14 @@ func StripPrefix(prefix string, h Handler) Handler {
 			}
 		}
 		if p := strings.TrimPrefix(path, prefix); len(p) < len(path) {
-			r2 := new(Request)
+			r2 := new(http.Request)
 			*r2 = *r
 			r2.URL = new(url.URL)
 			*r2.URL = *r.URL
 			r2.URL.Path = p
 			h.ServeHTTP(w, r2)
 		} else {
-			NotFound(w, r)
+			http.NotFound(w, r)
 		}
 	})
 }
